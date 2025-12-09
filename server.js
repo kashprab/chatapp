7c31184afd24cd81bdc02d440a711d06c46be7ca
Kash Prabhu <kashprabhu@dhcp-10-8-192-047.mobile.reshsg.uci.edu>
2025-12-08 23:03:00 -0800

    fixed double messages on client side

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ console.log("WebSocket server running on ws://localhost:8080");
 
 wss.on("connection", function connection(ws) {
     ws.on("message", function incoming(message) {
+        // Broadcast to all other connected clients (exclude the sender)
         wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message.toString());
             }
         });
